dc26decc944f11c21f2faa5ce5a312d6f6555687
serhatbek <serhat.bekk@gmail.com>
2022-08-18 13:45:32 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  const [tabItems, setTabItems] = useState();
+  console.log('tab items', tabItems);
+
+  const getTabsItems = async () => {
+    try {
+      const response = await axios(url);
+      const data = response.data;
+      setTabItems(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTabsItems();
+  }, []);
+
   return <div className='App'>Tabs</div>;
 }
 
